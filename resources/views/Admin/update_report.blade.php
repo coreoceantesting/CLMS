a4f45d5d9eaa2cc8dd9f1c59a12c68ef6715bda2
coreoceantesting <coreoceantesting@gmail.com>
2023-12-04 15:34:00 +0530

    add back button while updating report

--- resources/views/Admin/update_report.blade.php
+++ resources/views/Admin/update_report.blade.php
@@ -5,7 +5,7 @@
     <div class="container py-4">
         <div class="card">
             <div class="card-header">
-               <h1 class="text-center"><strong>Patient Details</strong></h1> 
+               <h1 class="text-center"><strong>Patient Details</strong><a class="btn btn-sm btn-primary" style="float: right" href="{{route('patient_pending_list')}}">Back</a></h1> 
             </div>
             <div class="card-body">
                 <table class="table table-bordered">
